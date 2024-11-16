271db99221710623338cb2c04976584e8a227380
KaliStudio <kalicem76@hotmail.fr>
2024-11-16 21:24:23 +0100

    Update Born2BeRoot_Tester.sh
    Uniquement Rocky!
    Clonez le référentiel Git dans votre machine virtuelle.
    curl -O https://raw.githubusercontent.com/42mdemare/Born2BeRoot/main/Born2BeRoot_Tester.sh
    Une fois téléchargé, donnez des permissions d'exécution au script si nécessaire :  chmod +x Born2BeRoot_Tester.sh
    Si vous utilisez un compte utilisateur normal, exécutez les fichiers Born2BeRoot_Tester.sh avec la commande sudo.
    Les fichiers cronjob détectent uniquement le fichier nommé monitoring.sh . Vous pouvez modifier le nom du fichier dans test.sh pour qu'il corresponde au nom de fichier de votre script de surveillance (uniquement si le vôtre est différent).
    Exécutez la commande en utilisant « sudo bash Born2BeRoot_Tester.sh » ou « sudo ./Born2BeRoot_Tester.sh »
    Assurez-vous d'exécuter en utilisant votre nom d'utilisateur. Pas en tant que ROOT ou en utilisant SU.

--- Born2BeRoot_Tester.sh
+++ Born2BeRoot_Tester.sh
@@ -11,14 +11,16 @@ CYAN='\033[0;96m'
 GRAY='\033[0;90m'
 WHITE='\033[0;97m'
 
+FAILED=0
+
 printf "${BLUE}______                   _____ ______     ______           _          \n${DEF_COLOR}";
 printf "${BLUE}| ___ \                 / __  \| ___ \    | ___ \         | |         \n${DEF_COLOR}";
 printf "${BLUE}| |_/ / ___  _ __ _ __  \/ / /'| |_/ / ___| |_/ /___   ___| | ___   _ \n${DEF_COLOR}";
-printf "${CYAN}| ___ \/ _ \| '__| '_ \   / /  | ___ \/ _ \    // _ \ / __| |/ / | | |\n${DEF_COLOR}";
-printf "${CYAN}| |_/ / (_) | |  | | | |./ /___| |_/ /  __/ |\ \ (_) | (__|   <| |_| |\n${DEF_COLOR}";
-printf "${CYAN}\____/ \___/|_|  |_| |_|\_____/\____/ \___\_| \_\___/ \___|_|\_\\__, |\n${DEF_COLOR}";
-printf "${CYAN}                                                                 __/ | Tester \n${DEF_COLOR}";
-printf "${CYAN}                                                                |___/  By Mdemare\n\n${DEF_COLOR}";
+printf "${BLUE}| ___ \/ _ \| '__| '_ \   / /  | ___ \/ _ \    // _ \ / __| |/ / | | |\n${DEF_COLOR}";
+printf "${BLUE}| |_/ / (_) | |  | | | |./ /___| |_/ /  __/ |\ \ (_) | (__|   <| |_| |\n${DEF_COLOR}";
+printf "${BLUE}\____/ \___/|_|  |_| |_|\_____/\____/ \___\_| \_\___/ \___|_|\_\\__, |\n${DEF_COLOR}";
+printf "${BLUE}                                                                 __/ | Tester \n${DEF_COLOR}";
+printf "${BLUE}                                                                |___/  By Mdemare\n\n${DEF_COLOR}";
 
 USER=$(whoami)
 
@@ -27,6 +29,10 @@ if [ $USER != "root" ]; then
   exit;
 fi
 
+printf "${BLUE}╔══════════════════════════════════════════════════════════════════════════════╗\n${DEF_COLOR}"
+printf "${BLUE}║                                Mandatory Tests                               ║\n${DEF_COLOR}"
+printf "${BLUE}╚══════════════════════════════════════════════════════════════════════════════╝\n${DEF_COLOR}"
+
 # Check if GUI mode is disabled
 printf "${MAGENTA}1. GUI MODE DISABLE?${DEF_COLOR}\n";
 RES=$(systemctl get-default)
@@ -34,6 +40,7 @@ if [[ $RES == "multi-user.target" ]]; then
   printf "${GREEN}[GOOD] ✔${GRAY} GUI mode disabled${DEF_COLOR}\n";
 else
   printf "${RED}[FAILED] ✗${GRAY} GUI mode enabled${DEF_COLOR}\n";
+  FAILED=$((FAILED + 1))
 fi
 
 # Hostname check
@@ -45,10 +52,10 @@ if [ "$RES" == "$EXPECTED_HOSTNAME" ]; then
   printf "${GREEN}[GOOD] ✔${DEF_COLOR}\n";
 else
   printf "${RED}[FAILED] ✗${DEF_COLOR}\n";
+  FAILED=$((FAILED + 1))
 fi
 
 # LVM and encrypted partitions
-# Vérification des partitions
 echo
 printf "${MAGENTA}3. Disk partitions${DEF_COLOR}\n";
 
@@ -58,6 +65,7 @@ if [ $RES -gt 1 ]; then
   printf "${GREEN}[GOOD] ✔${GRAY} LVM partitions detected${DEF_COLOR}\n"
 else
   printf "${RED}[FAILED] ✗${GRAY} No LVM partitions found${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
 fi
 
 RES=$(lsblk | grep home | wc -l)
@@ -65,6 +73,7 @@ if [ $RES -gt 0 ]; then
   printf "${GREEN}[GOOD] ✔${GRAY} Home partition detected${DEF_COLOR}\n"
 else
   printf "${RED}[FAILED] ✗${GRAY} No home partition found${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
 fi
 
 RES=$(lsblk | grep swap | wc -l)
@@ -72,6 +81,7 @@ if [ $RES -gt 0 ]; then
   printf "${GREEN}[GOOD] ✔${GRAY} Swap partition detected${DEF_COLOR}\n"
 else
   printf "${RED}[FAILED] ✗${GRAY} No swap partition found${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
 fi
 
 RES=$(lsblk | grep root | wc -l)
@@ -79,7 +89,139 @@ if [ $RES -gt 0 ]; then
   printf "${GREEN}[GOOD] ✔${GRAY} Root partition detected${DEF_COLOR}\n"
 else
   printf "${RED}[FAILED] ✗${GRAY} No root partition found${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
 fi
+
+# Firewalld configuration
+echo
+printf "${MAGENTA}4. Firewalld (Firewall)${DEF_COLOR}\n";
+systemctl is-active firewalld &>/dev/null && printf "${GREEN}[GOOD] ✔${GRAY} Firewalld active${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} Firewalld inactive${DEF_COLOR}\n"FAILED=$((FAILED + 1));
+firewall-cmd --list-ports | grep -q "4242" && printf "${GREEN}[GOOD] ✔${GRAY} Port 4242 open${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} Port 4242 closed${DEF_COLOR}\n"FAILED=$((FAILED + 1));
+
+# PAM configuration for password policy
+echo
+printf "${MAGENTA}6. Password policy${DEF_COLOR}\n";
+
+# minlen
+RES=$(grep -o "minlen=10" /etc/security/pwquality.conf)
+if [ "$RES" == "minlen=10" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} minlen${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} minlen${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# ucredit
+RES=$(grep -o "ucredit=-1" /etc/security/pwquality.conf)
+if [ "$RES" == "ucredit=-1" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} uppercase (ucredit)${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} uppercase (ucredit)${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# lcredit
+RES=$(grep -o "lcredit=-1" /etc/security/pwquality.conf)
+if [ "$RES" == "lcredit=-1" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} lowercase (lcredit)${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} lowercase (lcredit)${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# dcredit
+RES=$(grep -o "dcredit=-1" /etc/security/pwquality.conf)
+if [ "$RES" == "dcredit=-1" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} digit (dcredit)${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} digit (dcredit)${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# maxrepeat
+RES=$(grep -o "maxrepeat=3" /etc/security/pwquality.conf)
+if [ "$RES" == "maxrepeat=3" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} consecutive characters (maxrepeat)${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} consecutive characters (maxrepeat)${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# difok
+RES=$(grep -o "difok=7" /etc/security/pwquality.conf)
+if [ "$RES" == "difok=7" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} different characters (difok)${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} different characters (difok)${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# enforce_for_root
+RES=$(grep -o "enforce_for_root" /etc/security/pwquality.conf)
+if [ "$RES" == "enforce_for_root" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} enforce_for_root${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} enforce_for_root${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# reject_username
+RES=$(grep -o "reject_username" /etc/security/pwquality.conf)
+if [ "$RES" == "reject_username" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} reject_username${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} reject_username${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# PASS_MAX_DAYS
+RES=$(grep "^PASS_MAX_DAYS" /etc/login.defs | awk '{print $2}')
+if [ "$RES" == "30" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} PASS_MAX_DAYS = 30${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} PASS_MAX_DAYS${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# PASS_MIN_DAYS
+RES=$(grep "^PASS_MIN_DAYS" /etc/login.defs | awk '{print $2}')
+if [ "$RES" == "2" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} PASS_MIN_DAYS = 2${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} PASS_MIN_DAYS${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# PASS_WARN_AGE
+RES=$(grep "^PASS_WARN_AGE" /etc/login.defs | awk '{print $2}')
+if [ "$RES" == "7" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} PASS_WARN_AGE = 7${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} PASS_WARN_AGE${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# Vérification du dossier /var/log/sudo
+if [ -d "/var/log/sudo/" ]; then
+  printf "${GREEN}[GOOD] ✔${GRAY} folder /var/log/sudo exists${DEF_COLOR}\n"
+else
+  printf "${RED}[FAILED] ✗${GRAY} folder /var/log/sudo does not exist${DEF_COLOR}\n"
+  FAILED=$((FAILED + 1))
+fi
+
+# SSH Configuration
+echo
+printf "${MAGENTA}7. SSH Configuration${DEF_COLOR}\n";
+systemctl is-active sshd &>/dev/null && printf "${GREEN}[GOOD] ✔${GRAY} SSH active${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} SSH inactive${DEF_COLOR}\n" FAILED=$((FAILED + 1));
+semanage port -l | grep -q "4242" && printf "${GREEN}[GOOD] ✔${GRAY} Port 4242 allowed in SELinux${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} Port 4242 not allowed in SELinux${DEF_COLOR}\n" FAILED=$((FAILED + 1));
+
+# Monitoring script cron job
+echo
+printf "${MAGENTA}8. Cronjob for monitoring script${DEF_COLOR}\n";
+crontab -l | grep -q "monitoring.sh" && printf "${GREEN}[GOOD] ✔${GRAY} Monitoring script scheduled${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} Monitoring script missing in cron${DEF_COLOR}\n" FAILED=$((FAILED + 1));
+
+echo
+printf "${MAGENTA}3. BONNUS${DEF_COLOR}\n";
 
 # Vérification des partitions bonus
 echo
@@ -113,11 +255,9 @@ else
   printf "${RED}[FAILED] ✗${GRAY} No var-log partition found${DEF_COLOR}\n"
 fi
 
-# Firewalld configuration
-echo
-printf "${MAGENTA}4. Firewalld (Firewall)${DEF_COLOR}\n";
-systemctl is-active firewalld &>/dev/null && printf "${GREEN}[GOOD] ✔${GRAY} Firewalld active${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} Firewalld inactive${DEF_COLOR}\n";
-firewall-cmd --list-ports | grep -q "4242" && printf "${GREEN}[GOOD] ✔${GRAY} Port 4242 open${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} Port 4242 closed${DEF_COLOR}\n";
+printf "${BLUE}╔══════════════════════════════════════════════════════════════════════════════╗\n${DEF_COLOR}"
+printf "${BLUE}║                                   Bonus Tests                                ║\n${DEF_COLOR}"
+printf "${BLUE}╚══════════════════════════════════════════════════════════════════════════════╝\n${DEF_COLOR}"
 
 # Bonus: Web server and services
 echo
@@ -126,115 +266,15 @@ systemctl is-active lighttpd &>/dev/null && printf "${GREEN}[GOOD] ✔${GRAY} Li
 systemctl is-active mariadb &>/dev/null && printf "${GREEN}[GOOD] ✔${GRAY} MariaDB active${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} MariaDB inactive${DEF_COLOR}\n";
 firewall-cmd --list-ports | grep -q "80" && printf "${GREEN}[GOOD] ✔${GRAY} HTTP port 80 open${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} HTTP port 80 closed${DEF_COLOR}\n";
 
-# PAM configuration for password policy
+
+# Last message according to the results
 echo
-printf "${MAGENTA}6. Password policy${DEF_COLOR}\n";
-
-# minlen
-RES=$(grep -o "minlen=10" /etc/security/pwquality.conf)
-if [ "$RES" == "minlen=10" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} minlen${DEF_COLOR}\n"
+if [ $FAILED -eq 0 ]; then
+printf "${GREEN}╔══════════════════════════════════════════════════════════════════════════════╗\n${DEF_COLOR}"
+printf "${GREEN}║   🎉🥳  Mandatory and Bonus Tests Completed, your have Rockyed it! 🥳🎉    ║\n${DEF_COLOR}"
+printf "${GREEN}╚══════════════════════════════════════════════════════════════════════════════╝\n${DEF_COLOR}"
 else
-  printf "${RED}[FAILED] ✗${GRAY} minlen${DEF_COLOR}\n"
+printf "${RED}╔══════════════════════════════════════════════════════════════════════════════╗\n${DEF_COLOR}"
+printf "${RED}║ 😢💔 Some tests failed. 😞It's sad😢Please review the issues above. 💔😢  ║\n${DEF_COLOR}"
+printf "${RED}╚══════════════════════════════════════════════════════════════════════════════╝\n${DEF_COLOR}"
 fi
-
-# ucredit
-RES=$(grep -o "ucredit=-1" /etc/security/pwquality.conf)
-if [ "$RES" == "ucredit=-1" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} uppercase (ucredit)${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} uppercase (ucredit)${DEF_COLOR}\n"
-fi
-
-# lcredit
-RES=$(grep -o "lcredit=-1" /etc/security/pwquality.conf)
-if [ "$RES" == "lcredit=-1" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} lowercase (lcredit)${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} lowercase (lcredit)${DEF_COLOR}\n"
-fi
-
-# dcredit
-RES=$(grep -o "dcredit=-1" /etc/security/pwquality.conf)
-if [ "$RES" == "dcredit=-1" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} digit (dcredit)${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} digit (dcredit)${DEF_COLOR}\n"
-fi
-
-# maxrepeat
-RES=$(grep -o "maxrepeat=3" /etc/security/pwquality.conf)
-if [ "$RES" == "maxrepeat=3" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} consecutive characters (maxrepeat)${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} consecutive characters (maxrepeat)${DEF_COLOR}\n"
-fi
-
-# difok
-RES=$(grep -o "difok=7" /etc/security/pwquality.conf)
-if [ "$RES" == "difok=7" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} different characters (difok)${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} different characters (difok)${DEF_COLOR}\n"
-fi
-
-# enforce_for_root
-RES=$(grep -o "enforce_for_root" /etc/security/pwquality.conf)
-if [ "$RES" == "enforce_for_root" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} enforce_for_root${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} enforce_for_root${DEF_COLOR}\n"
-fi
-
-# reject_username
-RES=$(grep -o "reject_username" /etc/security/pwquality.conf)
-if [ "$RES" == "reject_username" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} reject_username${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} reject_username${DEF_COLOR}\n"
-fi
-
-# PASS_MAX_DAYS
-RES=$(grep "^PASS_MAX_DAYS" /etc/login.defs | awk '{print $2}')
-if [ "$RES" == "30" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} PASS_MAX_DAYS = 30${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} PASS_MAX_DAYS${DEF_COLOR}\n"
-fi
-
-# PASS_MIN_DAYS
-RES=$(grep "^PASS_MIN_DAYS" /etc/login.defs | awk '{print $2}')
-if [ "$RES" == "2" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} PASS_MIN_DAYS = 2${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} PASS_MIN_DAYS${DEF_COLOR}\n"
-fi
-
-# PASS_WARN_AGE
-RES=$(grep "^PASS_WARN_AGE" /etc/login.defs | awk '{print $2}')
-if [ "$RES" == "7" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} PASS_WARN_AGE = 7${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} PASS_WARN_AGE${DEF_COLOR}\n"
-fi
-
-# Vérification du dossier /var/log/sudo
-if [ -d "/var/log/sudo/" ]; then
-  printf "${GREEN}[GOOD] ✔${GRAY} folder /var/log/sudo exists${DEF_COLOR}\n"
-else
-  printf "${RED}[FAILED] ✗${GRAY} folder /var/log/sudo does not exist${DEF_COLOR}\n"
-fi
-
-# SSH Configuration
-echo
-printf "${MAGENTA}7. SSH Configuration${DEF_COLOR}\n";
-systemctl is-active sshd &>/dev/null && printf "${GREEN}[GOOD] ✔${GRAY} SSH active${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} SSH inactive${DEF_COLOR}\n";
-semanage port -l | grep -q "4242" && printf "${GREEN}[GOOD] ✔${GRAY} Port 4242 allowed in SELinux${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} Port 4242 not allowed in SELinux${DEF_COLOR}\n";
-
-# Monitoring script cron job
-echo
-printf "${MAGENTA}8. Cronjob for monitoring script${DEF_COLOR}\n";
-crontab -l | grep -q "monitoring.sh" && printf "${GREEN}[GOOD] ✔${GRAY} Monitoring script scheduled${DEF_COLOR}\n" || printf "${RED}[FAILED] ✗${GRAY} Monitoring script missing in cron${DEF_COLOR}\n";
-
-echo
-printf "${CYAN}Mandatory and Bonus Tests Completed, your have Rockyed!${DEF_COLOR}\n\n";
