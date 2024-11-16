3b7f92104c45c44fe79c1b87b4568c37166184c4
KaliStudio <kalicem76@hotmail.fr>
2024-11-16 13:23:45 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,4 +1,26 @@
+#!/bin/bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+ARCH=$(uname -m)
+DISTRIBUTION=$(awk '{print $1}' /etc/redhat-release)
+VERSION=$(awk '{print $4}' /etc/redhat-release)
+KERNEL=$(uname -r)
+CPU_PHYSICAL=$(lscpu | awk '/Socket\(s\):/ {print $2}')
+VCPU=$(lscpu | awk '/^CPU\(s\):/ {print $2}')
+RAM_USAGE=$(free -m | awk '/Mem:/ {printf "%.0f/%.0fMB (%.2f%%)", $3, $2, $3*100/$2}')
+DISK_USAGE=$(df -BG --output=size,used,pcent / | awk 'NR==2 {printf "%s/%s (%s)\n", $2, $1, $3}')
+CPU_LOAD=$(top -b -n1 | awk '/Cpu\(s\):/ {printf "%.1f%%", $2 + $4}')
+LAST_BOOT=$(who -b | awk '{print $3, $4}')
+LVM_STATUS=$(lsblk | grep -q "lvm" && echo "yes" || echo "no")
+TCP_CONNECTIONS=$(ss -tn state established | grep -c ESTAB)
+USER_LOG=$(who | wc -l)
+IP=$(hostname -I | awk '{print $1}')
+MAC=$(ip link show | awk '/ether/ {print $2}')
+SUDO_CMDS=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+
 MESSAGE="
+
 ####################################################################
 ##                                                                ##
 ##                      SYSTEM MONITORING REPORT                  ##
@@ -38,6 +60,5 @@ MESSAGE="
 ####################################################################
 "
 
-# Broadcast the message to all connected terminals
 echo "$MESSAGE" | wall 2>/dev/null
 echo ""
